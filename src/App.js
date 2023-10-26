28d1aac248bd10be216bfa7432de704b19ff45d5
Nutan Kumar <nutan2247@gmail.com>
2023-10-26 11:44:20 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Room from "./components/Room";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
